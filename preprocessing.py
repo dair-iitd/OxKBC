Added code that runs, but gives incorrect results

preprocessing.py
@@ -2,22 +2,25 @@ import os
 import sys
 import pickle
 import kb
-import settings
+# import settings
 import numpy as np
 import argparse
 
 def get_input(fact,y,template_obj_list):
     x=[]
+    # print(fact)
+    # print(fact)
     for template in template_obj_list:
+        # print(template.get_input(fact))
+        # print("aman")
         x.extend(template.get_input(fact))
     
     x.append(y)
+    # print(x,"**xxx***")
     return x
 
 #TODO write both txt and pickle file 
 def preprocess(kb,template_obj_list,negative_count=10):
-
-    template_obj_list=[]
 
     # for file in range(settings.templates):
     #     filepath=os.path.join(pickle_file_path,str(file)+'.pkl')
@@ -25,8 +28,13 @@ def preprocess(kb,template_obj_list,negative_count=10):
 
 
     new_facts=[]
+
+    ctr = 0
         
     for facts in kb.facts:
+        ctr += 1
+        if(ctr%100==0):
+            print("Processed ",ctr)
         ns = np.random.randint(0, len(kb.entity_map), negative_count)
         no = np.random.randint(0, len(kb.entity_map), negative_count)
 
@@ -37,7 +45,8 @@ def preprocess(kb,template_obj_list,negative_count=10):
             new_facts.append(get_input(new_fact,0,template_obj_list))
             new_fact=(facts[0],facts[1],no[neg_facts])
             new_facts.append(get_input(new_fact,0,template_obj_list))
-            
+    
+    # print(new_facts)
 
     dump=open('selection_module.data','w')
 
@@ -69,5 +78,7 @@ if __name__ == "__main__":
     kvalid,template_objs = main.main(dataset_root, args.model_weights, args.template_load_dir,
          None, args.model_type, args.t_ids, args.oov_entity)
 
+    print("Objs is ",template_objs)
+    # exit(0)
     preprocess(kvalid,template_objs)
         

sm/compute.py
@@ -18,6 +18,9 @@ import numpy as np
 import math
 from sklearn import metrics
 import settings
+from collections import Counter
+import math
+
 
 cuda = settings.cuda
 
@@ -36,17 +39,25 @@ class Loss(object):
     #returns - loss, ypred and ground truth 
     def __call__(self,var,model,mode):
         # Pdb.set_trace()
+        args = self.args
         x = var[0]
         y = var[1].squeeze()
         template_score = model(x)
         #batch_size x (num_templates + 1)
         max_score,arg_max_score = torch.max(template_score,dim=1)
-        loss_tensor  = (y == 1).float()*(arg_max_score == template_score.size(1)-1).float()*max_score*args.rho +
-                (y == 1).float()*(arg_max_score != template_score.size(1)-1).float()*max_score*args.mil_reward + 
-                (y == 0).float()*(arg_max_score != template_score.size(1)-1).float()*args.neg_reward
-                (y == 0).float()*(arg_max_score == template_score.size(1)-1).float()*args.pos_reward + 
+        # loss_tensor  = (y == 1).float()*(arg_max_score == template_score.size(1)-1).float()*max_score*args.rho + (y == 1).float()*(arg_max_score != template_score.size(1)-1).float()*max_score*args.mil_reward + (y == 0).float()*(arg_max_score != template_score.size(1)-1).float()*args.neg_reward + (y == 0).float()*(arg_max_score == template_score.size(1)-1).float()*args.pos_reward
+        loss_tensor  = (y == 1).float()*(arg_max_score == ((template_score.size(1)-1))).float()*max_score*args.rho + (y == 1).float()*(arg_max_score != (template_score.size(1)-1)).float()*max_score*args.mil_reward + (y == 0).float()*(arg_max_score != (template_score.size(1)-1)).float()*args.neg_reward + (y == 0).float()*(arg_max_score == (template_score.size(1)-1)).float()*args.pos_reward
+
+        # print((template_score.size(1)-1))
 
         loss = -1*loss_tensor.mean()
+        # print(loss_tensor)
+        # print(loss)
+        # print("Loss is ------> ",loss)
+        # exit(0)
+        if(math.isnan(loss)):
+            print(loss_tensor)
+            exit(0)
         return loss, arg_max_score, y  
 
 
@@ -71,6 +82,8 @@ def compute(epoch, model, loader, optimizer, mode, fh, tolog, eval_fn, args):
 
     #var is a list of - sentence, ner , pos and idx
     for var in loader:
+        # print(var)
+        # print(type(var))
         #Pdb().set_trace()
         var = list(var)
         idx = var[-1]
@@ -84,7 +97,7 @@ def compute(epoch, model, loader, optimizer, mode, fh, tolog, eval_fn, args):
             if cuda:
                 var[index] = var[index].cuda()
 
-        loss, arg_max_score, y   = eval_fn(var, model, mode) 
+        loss, arg_max_score, y   = eval_fn(var, model, mode)
 
         if mode == 'train':
             loss.backward()
@@ -93,9 +106,14 @@ def compute(epoch, model, loader, optimizer, mode, fh, tolog, eval_fn, args):
     
         # pos_pred = pos_pred.data.cpu().numpy()
         cum_loss = cum_loss + loss.data[0]*y.size(0)
-        cum_count += count 
+        # cum_count += count
+        cum_count = count
+        # print("cum_count is -->",cum_count)
+        # print("count is -->",count)
 
-        if (count - last_print) >= 20:
+
+        if (count - last_print) >= args.log_after:
+            print("Counts--> ", Counter(arg_max_score.data.numpy())) 
             last_print = count 
             rec = [epoch, mode, 1.0 * cum_loss / cum_count, cum_count, len(loader.dataset), time.time() - t1]
             utils.log(','.join([str(round(x, 6)) if isinstance(

sm/dataset.py
@@ -7,16 +7,23 @@ import utils
 from IPython.core.debugger import Pdb 
 import pickle
 import copy
-import data_samplers 
+import numpy as np
+# import data_samplers 
 import itertools
 
 def get_data_loaders(args):
-    return (SelectionModuleDataset(args.training_data_path),)
+    return DataLoader(SelectionModuleDataset(args.training_data_path),batch_size=args.batch_size,shuffle=True)
 
 
 class SelectionModuleDataset(torch.utils.data.Dataset):    
     def __init__(self, input_file_path):
-        self.data  = pickle.load(open(input_file_path,'rb'))
+        data = np.loadtxt(input_file_path,delimiter=',',dtype=float)
+        means = np.mean(data[:,:-1],axis=0).reshape(1,-1)
+        stds = np.std(data[:,:-1],axis=0).reshape(1,-1)
+        data[:,:-1] = (data[:,:-1]-means)/stds
+        self.data=data
+        print("Loaded data" , self.data)
+        # exit(0)
 
     def __len__(self):
         return len(self.data)

sm/models.py
@@ -26,10 +26,17 @@ class SelectionModule(nn.Module):
 
         module_list.append(nn.Sequential(nn.Linear(prev,output_size)))
         self.mlp=nn.ModuleList(module_list)
+        self.softmax = nn.Softmax(dim=1)
 
         if settings.cuda:
             self = self.cuda()
         
 
     def forward(self,x):
-        return self.mlp(x)
+        x = x.float()
+        # print(x)
+        # exit(0)
+        for layer in self.mlp:
+            x = layer(x)
+        return self.softmax(x)
+        # return self.mlp(x)

template2.py
@@ -84,7 +84,7 @@ class Template2(TemplateBaseClass):
 
         if(key in self.table.keys()):
             index_max = np.argmax(self.table[key].values())
-            max_score = list(self.table[key].values)[index_max]
+            max_score = list(self.table[key].values())[index_max]
             my_score = self.table[key].get(fact[2],0)
             simi = self.base_model.get_entity_similarity(fact[2],index_max)
             rank = utils.get_rank(self.table[key].values(),my_score)