Solved minor bug in embedding calculation and cleaned logging

sm/compute.py
@@ -22,7 +22,7 @@ class Loss(object):
         self.args = args
         self.criterion = nn.NLLLoss(weight=torch.Tensor([1.0, 20.0]))
 
-        l = [args.mil_reward for i in range(args.output_size-1)]
+        l = [args.mil_reward for i in range(args.num_templates)]
         l.insert(0, args.rho)
         self.weights = Variable(torch.Tensor(
             l).unsqueeze(0), requires_grad=False)
@@ -30,7 +30,7 @@ class Loss(object):
         self.labels = [x+1 for x in range(args.num_templates)]
         self.header = functools.reduce(
             lambda x, y: x+y, [[y+str(x) for y in ['p', 'r', 'f', 's']] for x in self.labels])
-        self.header.extend(['mip', 'mir', 'mif', 'acc'])
+        self.header.extend(['acc','mip', 'mir', 'mif'])
 
         if settings.cuda:
             self.criterion.cuda()

sm/main.py
@@ -2,6 +2,7 @@ import argparse
 import logging
 import os
 import pickle
+import pprint
 import time
 
 import numpy as np
@@ -15,6 +16,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import utils
+
 
 def main(args):
 
@@ -41,7 +43,7 @@ def main(args):
 
     logging.info('Beginning code for experiment {} and storing stuff in {}'.format(
         exp_name, args.output_path))
-    logging.info('Loaded arguments as {}'.format(str(args)))
+    logging.info('Loaded arguments as {}'.format(str(pprint.pformat(args))))
 
     # Begin of main code
 
@@ -138,21 +140,15 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', help='batch size',
                         type=int, default=256)
 
-    # Concatenation Model parameters
-    parser.add_argument('--input_size', help='Input size',
-                        type=int, default=35)
-    parser.add_argument('--output_size', help='output size',
-                        type=int, default=5)
-
-    # MIL Model parameters
-    parser.add_argument('--use_ids', help='Use embeddings of entity and relations while training',
-                        action='store_true', default=False)
-    parser.add_argument('--mil', help='Use MIL model',
-                        action='store_true', default=False)
+    # Model parameters
     parser.add_argument('--each_input_size',
                         help='Input size of each template', type=int, default=7)
     parser.add_argument(
         '--num_templates', help='number of templates excluding other', type=int, default=5)
+    parser.add_argument('--use_ids', help='Use embeddings of entity and relations while training',
+                        action='store_true', default=False)
+    parser.add_argument('--mil', help='Use MIL model',
+                        action='store_true', default=False)
 
     # Optimizer parameters
     parser.add_argument(

sm/models.py
@@ -11,7 +11,7 @@ def select_model(args):
     if args.mil:
         return SelectionModuleMIL(args.each_input_size, args.num_templates, args.hidden_unit_list, args.embed_size, args.use_ids)
     else:
-        return SelectionModule(args.input_size, args.output_size, args.hidden_unit_list, args.embed_size, args.use_ids)
+        return SelectionModule(args.each_input_size*args.num_templates, args.num_templates+1, args.hidden_unit_list, args.embed_size, args.use_ids)
 
 
 class SelectionModule(nn.Module):

sm/utils.py
@@ -33,7 +33,7 @@ def get_embed_size(base_model_file, use_ids):
         ent_embed = d['entity_real'].shape[1] + d['entity_type'].shape[1]
         rel_embed = d['head_rel_type'].shape[1] + \
             d['tail_rel_type'].shape[1] + d['rel_real'].shape[1]
-        return int(ent_embed+rel_embed)
+        return int(2*ent_embed+rel_embed)
     else:
         logging.error(
             'Base Model file not present at {}'.format(base_model_file))
@@ -48,8 +48,6 @@ def get_learning_rate(optimizer):
 def save_checkpoint(state, epoch, isBest, checkpoint_file, best_file):
     torch.save(state, checkpoint_file)
     if isBest:
-        print("isBest True. Epoch: {0}, bestError: {1}".format(
-            state['epoch'], state['best_score']))
         best_file = best_file + str(0)
         shutil.copyfile(checkpoint_file, best_file)
 