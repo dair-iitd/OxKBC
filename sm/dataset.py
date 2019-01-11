a96212fcc8d6617dcc682ac25e3ef542a70d9ec7
ankesh007 <ankeshgupta007@gmail.com>
2019-01-11 17:32:10 +0530

    Data loader modified for supporting labelled/unlabelled data

--- sm/dataset.py
+++ sm/dataset.py
@@ -9,17 +9,49 @@ from torch.utils.data import DataLoader, Dataset
 
 
 def get_data_loaders(args):
-    stats_file = args.training_data_path + '_stats'
-    train_loader = DataLoader(SelectionModuleDataset(args.training_data_path, base_model_file=args.base_model_file,
-                                                     each_input_size=args.each_input_size, use_ids=args.use_ids, mode='train', stats_file_path=stats_file), batch_size=args.batch_size, shuffle=True)
+    stats_file = os.path.join(args.output_path,'stats')
+    #stats_file = args.training_data_path + '_stats' 
+    train_loader = None 
+    #if args.supervision in ['un','semi']:
+    train_ds = SelectionModuleDataset(args.training_data_path, 
+                    base_model_file=args.base_model_file,
+                    each_input_size=args.each_input_size, 
+                    use_ids=args.use_ids, mode='train', 
+                    stats_file_path=stats_file)
+
+    train_loader = DataLoader(train_ds,batch_size=args.batch_size, shuffle=True)
 
     val_loader = None
     if args.val_data_path != '':
-        val_loader = DataLoader(SelectionModuleDataset(args.val_data_path, base_model_file=args.base_model_file, each_input_size=args.each_input_size,
-                                                       use_ids=args.use_ids, mode='eval', stats_file_path=stats_file), batch_size=args.batch_size, shuffle=False)
+        val_ds = SelectionModuleDataset(args.val_data_path, 
+                    base_model_file=args.base_model_file, 
+                    each_input_size=args.each_input_size,
+                    use_ids=args.use_ids, mode='eval', 
+                    stats_file_path=stats_file)
 
-    return (train_loader, val_loader)
+        val_loader = DataLoader(val_ds, batch_size=args.batch_size, shuffle=False)
+    
+    train_anot_loader = None
+    if args.supervision in ['semi','sup']:
+        #create another loader - train annotated
+        train_anot_ds = SelectionModuleDataset(args.labelled_training_data_path, 
+                    base_model_file=args.base_model_file,
+                    each_input_size=args.each_input_size, 
+                    use_ids=args.use_ids, mode='train', 
+                    stats_file_path=stats_file)
+        train_anot_loader = DataLoader(train_anot_ds, batch_size = args.batch_size, shuffle=True)
 
+
+    if args.supervision == 'un':
+        return (train_loader, val_loader, None)
+    elif args.supervision == 'semi':
+        return (train_loader, val_loader, train_anot_loader)
+    elif args.supervision == 'sup':
+        return (None, val_loader, train_anot_loader)
+    else:
+        raise
+
+    
 
 class SelectionModuleDataset(torch.utils.data.Dataset):
     def __init__(self, input_file_path, base_model_file, each_input_size=7, use_ids=False, mode='train', stats_file_path=None):
@@ -62,8 +94,10 @@ class SelectionModuleDataset(torch.utils.data.Dataset):
                 means[0, idx+4] = 0
                 stds[0, idx+3] = 1
                 stds[0, idx+4] = 1
+            #pickle.dump({'mean': means, 'std': stds},
+            #            open(input_file_path+'_stats', 'wb'))
             pickle.dump({'mean': means, 'std': stds},
-                        open(input_file_path+'_stats', 'wb'))
+                        open(stats_file_path, 'wb'))
             logging.info("Calculated stats of the given input file, dumped stats to {}".format(
                 input_file_path+'_stats'))
         else:
@@ -71,11 +105,11 @@ class SelectionModuleDataset(torch.utils.data.Dataset):
             means = stats['mean']
             stds = stats['std']
 
-        if(base_model_file != '' and os.path.exists(base_model_file)):
+        if(base_model_file is not None and os.path.exists(base_model_file)):
             with open(base_model_file, 'rb') as f:
                 self.bm = pickle.load(f)
             logging.info('Loaded base model from {}'.format(base_model_file))
-        else:
+        elif self.use_ids:
             logging.error(
                 'Base model file not present at {}'.format(base_model_file))
             raise Exception(
@@ -95,10 +129,6 @@ class SelectionModuleDataset(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.data)
-
-    # def save_raw_data(self,fh,predictions):
-        
-
 
     def __getitem__(self, idx):
         if(self.use_ids):
