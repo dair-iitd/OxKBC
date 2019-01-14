b91f99628dd278f68d4d23e2510cc48be6a0242c
ankesh007 <ankeshgupta007@gmail.com>
2019-01-14 15:04:49 +0530

    Automated naming log folder

--- sm/grid_search_cross_val.sh
+++ sm/grid_search_cross_val.sh
@@ -7,16 +7,16 @@ RHO=(-2 -1 -0.5 -0.25 -0.125 -0.05 -0.01 0 0.01 0.05 0.125 0.25 0.5)
 # NEG_REWARD=(-0.5)
 # RHO=(-5)
 
-dataset='fb15k'
+dataset='yago'
 hidden_unit_list="hidden_unit_list: [90,40]"
-logs="cross_val/cross_val_fb15k"
+supervision="sup"
+logs="cross_val/cross_val_"${dataset}"_90_40_"${supervision}
 mkdir -p $logs
 unlabelled_training_data_path='../logs/'${dataset}'/sm_with_id.data.pkl'
 labelled_training_data_path='../logs/'${dataset}'/exp_words/sm_valid_with_id.pkl'
 # val_data_path='../logs/'${dataset}'/exp_words/sm_sup_valid_with_id.pkl'
 base_model_file='../dumps/'${dataset}'_distmult_dump_norm.pkl'
 folds=5
-
 # cd $folder_path
 for i in "${NEG_REWARD[@]}"
 do
@@ -36,7 +36,7 @@ do
 		echo "neg_reward: $i" >> $yml
 		echo "rho: $j" >> $yml
 		echo "${hidden_unit_list}" >> $yml
-		echo "/home/cse/btech/cs1150210/anaconda3/bin/python3 cross_validation.py --folds ${folds} --dir ${dir} --labelled_training_data_path $labelled_training_data_path --unlabelled_training_data_path ${unlabelled_training_data_path} --num_epochs 20 --config $yml --batch_size 2048 --num_templates 5 --each_input_size 7 --supervision semi &" >> $sh
+		echo "/home/cse/btech/cs1150210/anaconda3/bin/python3 cross_validation.py --folds ${folds} --dir ${dir} --labelled_training_data_path $labelled_training_data_path --unlabelled_training_data_path ${unlabelled_training_data_path} --num_epochs 20 --config $yml --batch_size 2048 --num_templates 5 --each_input_size 7 --supervision ${supervision} &" >> $sh
 		echo "pids[${counter}]=""$""!" >> $sh
 		counter=$((counter+1))
 		# qsub -P cse $temp
