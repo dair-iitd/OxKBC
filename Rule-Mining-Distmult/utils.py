cdc1e01a24f22eb4a75240a2148b68cf81c46b67
ankesh007 <ankeshgupta007@gmail.com>
2019-02-06 17:57:13 +0530

    Corrected code / added code to generate explanation

--- Rule-Mining-Distmult/utils.py
+++ Rule-Mining-Distmult/utils.py
@@ -80,23 +80,56 @@ def get_ent_to_rel(data_arr):
     return ent_to_rel
 
 def load_pickle(filepath):
-	with open(filepath,"rb") as f:
-		dump=pickle.load(f)
-	return dump
+    with open(filepath,"rb") as f:
+        dump=pickle.load(f)
+    return dump
 
 def dump_pickle(dump,filepath):
-	with open(filepath,"wb") as f:
-		pickle.dump(dump,f)
+    with open(filepath,"wb") as f:
+        pickle.dump(dump,f)
 
 def get_relation_dict(data_arr):
-	relation_dict={}
-	count={}
+    relation_dict={}
+    count={}
 
-	for data in data_arr:
-	    if data[1] not in relation_dict:
-	    	relation_dict[data[1]]=[]
-	    	count[data[1]]=0
-	    relation_dict[data[1]].append((data[0],data[2]))
-	    count[data[1]]+=1
+    for data in data_arr:
+        if data[1] not in relation_dict:
+            relation_dict[data[1]]=[]
+            count[data[1]]=0
+        relation_dict[data[1]].append((data[0],data[2]))
+        count[data[1]]+=1
 
-	return count,relation_dict    
+    return count,relation_dict    
+
+def get_r_e1e2_dict(data_arr):
+    relation_dict={}
+
+    for data in data_arr:
+        if data[1] not in relation_dict:
+            relation_dict[data[1]]={}
+        pair=(data[0],data[2])
+        if pair not in relation_dict[data[1]]:
+            relation_dict[data[1]][pair]=-1
+
+    return relation_dict    
+
+
+def get_r1r2_e1e2_dict(data_arr, index_head):
+
+    count_r1_r2 = {}
+
+    for data in data_arr:
+        if(data[2] not in index_head):
+            continue
+        rel_tail_list = index_head[data[2]]
+        for rel_tail in rel_tail_list:
+            rel = rel_tail[0]
+            tail = rel_tail[1]
+            pair = (data[1], rel)
+            if(pair not in count_r1_r2):
+                count_r1_r2[pair] = {}
+            ent_pair=(data[0],tail)
+            if(ent_pair not in count_r1_r2[pair]):
+                count_r1_r2[pair][ent_pair]=data[2]
+
+    return count_r1_r2
